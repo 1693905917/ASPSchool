2cc13ff800c4de48f5eebcc097a6c4e6297019b1
1693905917 <1693905917@qq.com>
2024-06-27 14:14:28 +0800

    commit15.0

---
+++ 实验二参考代码/functions/mytool1.c
@@ -0,0 +1,8 @@
+#include "mytool1.h"
+#include <stdio.h>
+
+void mytool1_printf(char* print_str)
+{
+
+ printf("This is mytool1 print : %s", print_str);
+}
---
+++ 实验二参考代码/main.c
@@ -0,0 +1,10 @@
+#include "mytool1.h"
+#include "mytool2.h"
+
+int main()
+{
+ mytool1_printf("hello mytool1!\n");
+ mytool2_printf("hello mytool2!\n");
+ return 0;
+
+}
