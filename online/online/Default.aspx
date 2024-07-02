06013b03f7a340f2929d03f62d2a20a40f917dc4
1693905917 <1693905917@qq.com>
2024-07-02 21:51:38 +0800

    commit19.0

---
+++ online/online/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="online._Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>在线人数统计</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>在线人数统计</h2>
+            <asp:Label ID="lblOnlineUsersCount" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ online/online/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace online
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // 检查并增加在线人数
+                if (Session["OnlineUsersCount"] == null)
+                {
+                    Session["OnlineUsersCount"] = 1;
+                }
+                else
+                {
+                    Session["OnlineUsersCount"] = (int)Session["OnlineUsersCount"] + 1;
+                }
+
+                // 更新页面上的在线人数显示
+                UpdateOnlineUsersCountLabel();
+            }
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            // 减少在线人数
+            if (Session["OnlineUsersCount"] != null)
+            {
+                Session["OnlineUsersCount"] = (int)Session["OnlineUsersCount"] - 1;
+                UpdateOnlineUsersCountLabel();
+            }
+        }
+
+        private void UpdateOnlineUsersCountLabel()
+        {
+            // 显示当前在线人数
+            int onlineUsersCount = (int)Session["OnlineUsersCount"];
+            lblOnlineUsersCount.Text = $"当前在线人数：{onlineUsersCount}";
+        }
+    }
+}
