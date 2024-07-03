da971f20a6d35f094ab295d1cdded1a38621c8a4
1693905917 <1693905917@qq.com>
2024-07-03 23:08:43 +0800

    commit21.0

---
+++ studentData/studentData/Default.aspx
@@ -0,0 +1,24 @@
+﻿
+<!DOCTYPE html>
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="studentData._Default" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>学生信息表</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>本班学生信息表</h2>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+                CssClass="table table-bordered table-striped">
+                <Columns>
+                    <asp:BoundField DataField="StudentID" HeaderText="学生ID" />
+                    <asp:BoundField DataField="Name" HeaderText="姓名" />
+                    <asp:BoundField DataField="Age" HeaderText="年龄" />
+                    <asp:BoundField DataField="Grade" HeaderText="成绩" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ studentData/studentData/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace studentData
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // 模拟从数据库或其他数据源中获取学生信息
+                List<Student> students = GetStudentData();
+
+                // 绑定数据到GridView控件
+                GridView1.DataSource = students;
+                GridView1.DataBind();
+            }
+        }
+
+        // 模拟从数据源中获取学生信息的方法
+        private List<Student> GetStudentData()
+        {
+            List<Student> students = new List<Student>();
+
+            // 添加一些学生信息（这里模拟硬编码的方式，实际中可以从数据库或其他地方获取）
+            students.Add(new Student { StudentID = 1, Name = "张三", Age = 20, Grade = "A" });
+            students.Add(new Student { StudentID = 2, Name = "李四", Age = 21, Grade = "B" });
+            students.Add(new Student { StudentID = 3, Name = "王五", Age = 19, Grade = "A-" });
+            students.Add(new Student { StudentID = 4, Name = "赵六", Age = 22, Grade = "B+" });
+            students.Add(new Student { StudentID = 4, Name = "山东哥布林", Age = 22, Grade = "C-" });
+            return students;
+        }
+
+        // 定义一个学生类
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Grade { get; set; }
+        }
+    }
+}
